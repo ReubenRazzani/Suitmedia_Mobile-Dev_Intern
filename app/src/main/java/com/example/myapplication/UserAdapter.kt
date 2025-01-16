12198aaf74ac7529bb6f64e49e7116073e054a2e
Razzannn <razzanireuben@gmail.com>
2025-01-16 13:01:47 +0700

    Update App 4

--- app/src/main/java/com/example/myapplication/UserAdapter.kt
+++ app/src/main/java/com/example/myapplication/UserAdapter.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 
 class UserAdapter(private val userList: MutableList<User>, private val onItemClick: (User) -> Unit) :
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -14,17 +13,10 @@ class UserAdapter(private val userList: MutableList<User>, private val onItemCli
     inner class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvName: TextView = view.findViewById(R.id.tv_name)
         val tvEmail: TextView = view.findViewById(R.id.tv_email)
-        val ivAvatar: ImageView = view.findViewById(R.id.iv_avatar)
 
         fun bind(user: User) {
             tvName.text = "${user.firstName} ${user.lastName}"
             tvEmail.text = user.email
-
-            Glide.with(ivAvatar.context)
-                .load(user.avatar)
-                .into(ivAvatar)
-
-            itemView.setOnClickListener { onItemClick(user) }
         }
     }
 
